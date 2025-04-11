9d1939629151b2d2ad81e885338b8f650545d5d7
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2025-04-10 22:21:14 -0700

    Agents from config

--- src/models/agents.py
+++ src/models/agents.py
@@ -1,6 +1,11 @@
-from models import llm_call_messages, llm_call_messages_async
+from models import llm_call_messages, llm_call_messages_async, text_model
 from tools import Tool
 import asyncio
+from pydantic import BaseModel
+
+class AgentConfig(BaseModel):
+    name: str
+    system_prompt: str
 
 class Agent:
     def __init__(
@@ -8,7 +13,7 @@ class Agent:
         name: str,
         system_prompt: str,
         tools: list[Tool],
-        model: str = "openrouter/optimus-alpha",
+        model: str = text_model,
     ):
         self.name = name
         self.system_prompt = system_prompt
@@ -16,6 +21,10 @@ class Agent:
         self.messages = [{"role": "system", "content": self.system_prompt}]
         self.tools = tools
         self.data = []
+
+    @staticmethod
+    def from_config(config: AgentConfig, model: str = text_model):
+        return Agent(config.name, config.system_prompt, [], model)
 
     def call(self, prompt: str):
         self.messages.append({"role": "user", "content": prompt})
@@ -34,7 +43,16 @@ if __name__ == "__main__":
         name="Test123",
         system_prompt="You are a helpful assistant that speaks in the style of a pirate.",
         tools=[],
-        model="openrouter/optimus-alpha"
+        model=text_model
     )
     print(agent.call("What is the capital of France?"))
     print(asyncio.run(agent.call_async("What was the last thing you said?")))
+
+    config = AgentConfig(
+        name="Test1234",
+        system_prompt="You are a helpful assistant that speaks in haikus.",
+    )
+
+    agent2 = Agent.from_config(config)
+    print(agent2.call("What is the capital of Italy?"))
+    print(asyncio.run(agent2.call_async("What was the last thing you said?")))
