399a947c19c08e6776ae6e9200909b1b147716ae
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2025-04-10 21:59:13 -0700

    Basic tool class

---
+++ src/models/tools.py
@@ -0,0 +1,20 @@
+from typing import Callable, Dict
+from pydantic import BaseModel
+
+
+class Tool:
+    def __init__(self, name: str, description: str, function: Callable, argument_schema: BaseModel):
+        self.name = name
+        self.description = description
+        self.function = function
+        self.argument_schema = argument_schema
+
+    def __call__(self, arguments: dict) -> str:
+        if not self.argument_schema.model_validate(arguments):
+            return f"Invalid arguments: {arguments}"
+        return self.function(**arguments)
+
+    def __str__(self) -> str:
+        return f"{self.name}: {self.description}\nArguments: {self.argument_schema}"
+
+
